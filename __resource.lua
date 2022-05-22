bfe8843600265d309f5962b393ebf60f1421c8b0
PoloFivem <106012206+PoloFivem@users.noreply.github.com>
2022-05-22 11:27:00 +0200

    Fivem Loadinscreen | Free

---
+++ __resource.lua
@@ -0,0 +1,17 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+loadscreen 'index.html'
+
+
+files {
+    'index.html',
+    'stylesheet.css',
+    'config.js',
+    'config.css',
+    'app.js',
+    'imgs/logo.png',
+    'fonts/Sansation_Regular_0.ttf',
+    'fonts/Sansation_Light.ttf'
+
+
+}
---
+++ config.js
@@ -0,0 +1,17 @@
+﻿
+var config = {
+    "transitionInterval": 0,
+    "imgInterval": 8000,
+    "music": true,
+ 
+    "videoID": "ym4c711LseE",
+    
+    "musicVolume": 5,
+    "text": {
+        "title": "",
+        "link": "Swecity Roleplay är en välutvecklad server med trevliga och välkommnande staffs, ha de gött här inne!",
+
+    },
+    
+    "images": ['']
+}
